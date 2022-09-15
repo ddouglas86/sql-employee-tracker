004f7e9559260c8ac26a1767ad956424b8e4d58e
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-09-14 22:01:35 -0500

    added seeds for department and role

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES ("Corporate"),
+("Accounting"),
+("Marketing"),
+("Sales"),
+("PeopleOps"),
+("IT"),
+("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 500000, 1),
+("CFO", 450000, 1),
+("Accountant", 70000, 2),
+("Sr. Digital Marketer", 180000, 3),
+("Digital Marketer", 90000, 3),
+("Sr. Accountant", 100000, 2),
+("Designer", 55000, 3),
+("Inbound Consultant", 47000, 4),
+("Sr. Councel", 200000, 7),
+("Assistant", 110000, 1),
+("Chat Consultant", 40000, 4),
+("Sr. Developer", 110000, 6),
+("Manager", 70000, 4);
+
+
+
