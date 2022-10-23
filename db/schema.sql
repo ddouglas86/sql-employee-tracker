b98c562108db16c12eb47964453f58e35219ecef
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-10-23 15:45:43 -0500

    fixed error causing employee table to not be created. Updated where refrence to manager_id is read

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,6 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -23,11 +22,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT REFERENCES employee(id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -11,16 +11,28 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("CEO", 500000, 1),
 ("CFO", 450000, 1),
 ("Accountant", 70000, 2),
-("Sr. Digital Marketer", 180000, 3),
-("Digital Marketer", 90000, 3),
+("Sr. Digital Manager", 180000, 3),
+("Data Analyst", 95000, 3),
 ("Sr. Accountant", 100000, 2),
 ("Designer", 55000, 3),
 ("Inbound Consultant", 47000, 4),
-("Sr. Councel", 200000, 7),
+("Sr. Counsel", 200000, 7),
 ("Assistant", 110000, 1),
 ("Chat Consultant", 40000, 4),
-("Sr. Developer", 110000, 6),
-("Manager", 70000, 4);
+("Sr. Developer", 190000, 6),
+("Manager", 70000, 4),
+("VP", 100000, 1),
+("Recruiter", 74000, 6);
 
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Josie", "Frost", 1, NULL),
+("Michael", "McAulay", 2, NULL),
+("Jemma", "Mahone", 5, 9),
+("Pamela", "Thatchet", 9, NULL),
+("John", "Carrot", 10, 1),
+("Regina", "Kay", 4, 1),
+("Raeshel", "McDaniels", 13, 8),
+("Jamie", "Folger", 14, 2),
+("Tom", "Fellows", 14, 1),
+("Macy", "Sorbet", 11, 7);
 
